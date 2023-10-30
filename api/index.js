f9a427c70e31ca7f291d19db26b57bc708d06b07
nasteho bashir <nasteho764@gmail.com>
2023-10-30 21:06:39 +0300

    create user model

--- api/index.js
+++ api/index.js
@@ -1,7 +1,18 @@
-const express = require('express')
+import express from'express'
+import mongoose from 'mongoose';
+import dotenv from "dotenv"
 
+dotenv.config()
 const app = express()
 
+mongoose
+.connect(process.env.MONGO)
+.then(()=>{
+    console.log("connected MongoDb");
+})
+.catch((err)=>{
+    console.log(err);
+})
 app.listen(3000,()=>{
     console.log("server is runnning on port 3000 !!!");
 })
