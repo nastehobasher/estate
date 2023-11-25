f91900f949c9810848d3f0034a2e607603a40e12
nasteho bashir <nasteho764@gmail.com>
2023-11-25 19:21:26 +0300

    create sign in api

--- api/controllers/authCtrl.js
+++ api/controllers/authCtrl.js
@@ -1,5 +1,8 @@
+import jwt  from 'jsonwebtoken'
 import User from '../models/user.model.js'
 import bcrypt from'bcrypt'
+
+
 export const signup = async(req,res,next) => {
     const {username,email,password} =req.body
     // encrypted password
@@ -8,6 +11,23 @@ export const signup = async(req,res,next) => {
     try{
         await newUser.save()
         res.status(201).json("signup created succesfuly")
+    }catch(error){
+        next(error)
+    }
+}
+export const signIn = async(req,res,next) => {
+    const {email,password} =req.body
+    // encrypted password
+    try{
+        const findUser = await User.findOne({email})
+        if(!findUser) return next(errorHandler(404,'User not found!'))
+        const validPassword = bcrypt.compareSync(password,findUser.password)
+        if(!validPassword) return next(errorHandler(401,'Wrong cridentials!'))
+        const token = jwt.sign({id:findUser._id},process.env.JWT_SECRET);
+
+        // we should encrypt the password
+        const {password:pass, ...rest } = findUser._doc;
+        res.cookie('access_token',token,{ httpOnly: true}).status(200).json(rest)
     }catch(error){
         next(error)
     }
--- api/routes/authroute.js
+++ api/routes/authroute.js
@@ -1,8 +1,9 @@
 import express from 'express'
-import { signup } from '../controllers/authCtrl.js';
+import { signIn, signup } from '../controllers/authCtrl.js';
 
 const router = express.Router()
 
 router.post('/signup',signup)
+router.get('/signIn',signIn)
 
 export default router;
