5d6007a5b954e3e9001ff82c567a061eee1fd9b4
nasteho bashir <nasteho764@gmail.com>
2023-12-10 06:55:00 +0300

    complete profile page UI

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+
 
 export default function Profile() {
+  const { currentUser } = useSelector((state) => state.user)
+
+  console.log(currentUser);
   return (
-    <div>
-      
+      <div className='p-3 max-w-lg mx-auto'> 
+      <h1 className='text-3xl text-center font-semibold my-7'>Profile</h1>
+      <form className='flex flex-col gap-4'>
+        <img className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2' src={currentUser.avatar} alt='profile'/>
+        <input type='text' placeholder='username' id='username' className='border p-3 rounded-lg'/>
+        <input type='text' placeholder='email' id='email' className='border p-3 rounded-lg'/>
+        <input type='text' placeholder='password' id='password' className='border p-3 rounded-lg'/>
+
+        <button className='bg-slate-700 p-3 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>update</button>
+        {/* <button className='bg-green-700 p-3 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>create listing</button> */}
+      </form>
+      <div className='flex justify-between mt-5'>
+          <span className='text-red-700 cursor-pointer'>Delete Account</span>
+          <span className='text-red-700 cursor-pointer'>Sign out</span>
+        </div>
     </div>
+    // </div>
   )
 }
