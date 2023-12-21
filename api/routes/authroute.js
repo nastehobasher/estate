18e297ec19349b9392c77a7d57d09b438e235ddf
nasteho bashir <nasteho764@gmail.com>
2023-12-21 15:04:41 +0300

    add signout functionality

--- api/routes/authroute.js
+++ api/routes/authroute.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { google, signIn, signup } from "../controllers/authCtrl.js";
+import { google, signIn, signOut, signup } from "../controllers/authCtrl.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/signIn", signIn);
 router.post("/google", google);
+router.get("/signout", signOut);
 
 export default router;
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 import { useRef,useState,useEffect } from 'react'
 import {getDownloadURL, getStorage, ref, uploadBytesResumable} from 'firebase/storage'
 import { app } from '../firebase'
-import { updateUserStart,updateUserFailure,updateUserSuccess, deleteUserFailure, deleteUserStart, deleteUserSuccess } from '../redux/user/userSlice'
+import { updateUserStart,updateUserFailure,updateUserSuccess, deleteUserFailure, deleteUserStart, deleteUserSuccess, signOutUserStart, signOutUserFailure, signOutUserSuccess } from '../redux/user/userSlice'
 
 export default function Profile() {
   const fileRef = useRef(null)
@@ -95,6 +95,21 @@ export default function Profile() {
       dispatch(deleteUserFailure(error.message))
     }
   }
+  const handelSignOut = async ()=>{
+    try{
+      dispatch(signOutUserStart())
+      const res = await fetch('/api/auth/signout')
+      const data = await res.json()
+      if(data.success === false){
+        dispatch(deleteUserFailure(data.message))
+        return
+      }
+
+      dispatch(deleteUserSuccess(data))
+    }catch(error){
+      dispatch(deleteUserFailure(error.message))
+    }
+  }
   return (
       <div className='p-3 max-w-lg mx-auto'> 
       <h1 className='text-3xl text-center font-semibold my-7'>Profile</h1>
@@ -150,7 +165,7 @@ export default function Profile() {
       </form>
       <div className='flex justify-between mt-5'>
           <span onClick={handleDeleteUser} className='text-red-700 cursor-pointer'>Delete Account</span>
-          <span className='text-red-700 cursor-pointer'>Sign out</span>
+          <span onClick={handelSignOut} className='text-red-700 cursor-pointer'>Sign out</span>
       </div>
         {/* <p className='text-red-700 mt-5'>{ error ? error : ''}</p> */}
         <p className='text-green-700 mt-5'>{ error ? 'user is updated succesfuly' : ''}</p>
