d058aecc0db634ef4fb7ce8084483b040d670813
nasteho bashir <nasteho764@gmail.com>
2024-01-30 07:13:06 +0300

    add show more button functionlity

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -15,8 +15,9 @@ export default function Search() {
   });
   const [loading, setLoading] = useState(false);
   const [listings, setListings] = useState([]);
+  const [showMore, setShowMore] = useState(false);
 
-  console.log(listings);
+  // console.log(listings);
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
     const searchTermFormUrl = urlParams.get("searchTerm");
@@ -52,6 +53,11 @@ export default function Search() {
       const searchQuery = urlParams.toString();
       const res = await fetch(`api/listing/get?${searchQuery}`);
       const data = await res.json();
+      if(data.length > 8){
+        setShowMore(true)
+      }else{
+        setShowMore(false)
+      }
 
       setListings(data);
       setLoading(false);
@@ -107,6 +113,20 @@ export default function Search() {
     const searchQuery = urlParams.toString();
     navigate(`/search?${searchQuery}`);
   };
+
+  const onShowMoreClick = async()=>{
+    const numberOflistings = listings.length
+    const startIndex = numberOflistings
+    const urlParams = new URLSearchParams(location.search)
+    urlParams.set('startIndex',startIndex)
+    const searchQuery = urlParams.toString()
+    const res =await fetch(`/api/listing/get?${searchQuery}`)
+    const data = await res.json()
+    if(data.length < 9){
+      setShowMore(false)
+    }
+    setListings([...listings, ...data])
+  }
   return (
     <div className="flex flex-col md:flex-row">
       <div className="p-7 border-b-2 md:border-r-2 md:min-h-screen">
@@ -230,6 +250,10 @@ export default function Search() {
             listings.map((listing) => (
               <ListingItem key={listing._id} listing={listing} />
             ))}
+
+            {showMore && (
+              <button onClick={onShowMoreClick()} className="text-green-700 hover:underline p-7 text-center w-full"> Show more </button>
+            )}
         </div>
       </div>
     </div>
